8b27d20c2e0bbac13c719987beb4026d0ae93fff
Andrew <juwon1@ualberta.ca>
2016-12-19 21:58:00 -0700

    a

---
+++ 1.4/A_Real_Challenge/notreally.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+  long long a;
+  cin >> a;
+  cout << setprecision(100) << 4*sqrt(a) << endl;
+  return 0;
+}
