582ae6870cde0c7037aaf7965322a352507cdf2c
Andrewjjj <Juwon1@ualberta.ca>
2017-03-06 11:42:07 -0700

    Reverse Rot

--- Python3/1.5/Reverse_Rot/reverserot.py
+++ Python3/1.5/Reverse_Rot/reverserot.py
@@ -1,11 +1,29 @@
+d1=dict() #alph to num
+d2=dict() #num to alph
+msgstr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ_.'
+q=0
+for i in msgstr:
+    d1[i]=q
+    d2[q]=i
+    q+=1
+# print (d1)
+# print (d2)
+
+
 while True:
     a=input().split()
     fwd = int(a[0])
     if fwd==0:
         break
 
-    msg = [ord(w) for w in a[1]]
+    msg = [d1[w] for w in a[1]]
+    # print("1",msg)
     for i in range(len(msg)):
         msg[i]+=fwd
-    msg = [chr(a) for a in msg]
-    print(msg)
+        msg[i]=msg[i]%28
+    msg = [d2[a] for a in msg]
+    # print("2",msg)
+    msg.reverse()
+    # print("3",msg)
+    w="".join(msg)
+    print(w)
